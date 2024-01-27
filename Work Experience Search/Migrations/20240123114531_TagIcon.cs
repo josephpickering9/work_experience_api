1a521384e42c1568eb7e2fdf76acd2e9cb75b3fa
Joseph Pickering <josephpickering9@gmail.com>
2024-01-27 15:07:33 +0000

    Include migrations

---
+++ Work Experience Search/Migrations/20240123114531_TagIcon.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Work_Experience_Search.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagIcon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Colour",
+                table: "Tag",
+                newName: "Icon");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Icon",
+                table: "Tag",
+                newName: "Colour");
+        }
+    }
+}
