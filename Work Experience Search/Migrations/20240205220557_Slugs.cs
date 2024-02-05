8371278fd9270524ca3432338d2c41d2dbb25aa6
Joseph Pickering <josephpickering9@gmail.com>
2024-02-05 22:44:43 +0000

    Include migrations

---
+++ Work Experience Search/Migrations/20240205220557_Slugs.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Work_Experience_Search.Migrations
+{
+    /// <inheritdoc />
+    public partial class Slugs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Tag",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Project",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Company",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Tag");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Project");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Company");
+        }
+    }
+}
