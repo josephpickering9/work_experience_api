deec99fca41c8ad93e1bf20ffcb28ebd2f3ee346
Joseph Pickering <josephpickering9@gmail.com>
2024-02-09 15:47:30 +0000

    Show Mockup

---
+++ Work Experience Search/Migrations/20240209153007_ProjectShowMockup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Work_Experience_Search.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectShowMockup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowMockup",
+                table: "Project",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShowMockup",
+                table: "Project");
+        }
+    }
+}
