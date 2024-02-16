4efd8df548fab5b086f9e876def9939e348a4507
Joseph Pickering <josephpickering9@gmail.com>
2024-02-16 12:57:46 +0000

    Latest migration

--- WorkExperienceSearch/Migrations/20240216125620_ProjectImageProjectId.Designer.cs
+++ WorkExperienceSearch/Migrations/20240216125620_ProjectImageProjectId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Work_Experience_Search.Services.Database;
@@ -11,9 +12,11 @@ using Work_Experience_Search.Services.Database;
 namespace Work_Experience_Search.Migrations
 {
     [DbContext(typeof(Database))]
-    partial class DatabaseModelSnapshot : ModelSnapshot
+    [Migration("20240216125620_ProjectImageProjectId")]
+    partial class ProjectImageProjectId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
